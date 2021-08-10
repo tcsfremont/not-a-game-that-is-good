ce72719451e3a07d91bc186e52530462895a1d8f
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-08-10 09:35:05 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+import sys
+
+
+for index in range(len(sys.argv)):
+    if sys.argv[index] == '_':
+        sys.argv[index] = imput('enter a word: ')
+sys.argv.pop(0)    
+result = ' '.join(sys.argv
+    
+
+
+print(result)
+print(sys.argv
+   
+
+    
