926e9f223503263d203eb2ad86e1de54022c6d0a
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-08-11 09:11:35 -0700

    cleaning data

---
+++ encipher.py
@@ -0,0 +1,27 @@
+from sys import argv as arguments
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+if len(arguments) != 3:
+
+    print("2 arguments required!")
+    exit()
+
+
+from_filename = arguments[1]
+to_filename = arguments[2]
+print("imput " +from_filename)
+print("output: " + to_filename)
+from_file = open(from_filename, 'r', encoding='utf8')
+to_file = open(to_filename, 'w', encoding='utf8')
+
+contents = from_file .read()
+
+contents = [letter if letter.upper() in ALPHABET else '' for letter in contents]
+
+
+contents = ''.join(contents)
+contents = contents.upper()
+
+to_file.write(contents)
+
+from_file.close()
+to_file.close()
---
+++ frequency.py
@@ -0,0 +1,48 @@
+import sys
+
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+
+args = sys.argv[1:]
+
+for arg in args:
+
+
+    print(arg)
+text = ' '.join(args)
+print(text)
+
+lowercase_letter = text[0].lower()
+print(lowercase_letter)
+
+lowercase_text = [letter.lower() for letter in text]
+print(lowercase_letter)
+lowercase_text = [letter if letter in ALPHABET else '' for letter in lowercase_text]
+print(lowercase_text)
+
+lowercase_text=''.join(lowercase_text)
+print(lowercase_text)
+
+
+frequencies = {}
+for letter in ALPHABET:
+    frequencies[letter] = 0
+for letter in lowercase+_text:
+    if letter not in frequencies:
+        frequencies[letter] = 1
+    else:
+        frequencies[letter] += 1
+        
+
+
+
+
+
+print(frequencies)
+for letter in ALPHABET:
+    
+    count = frequencies[letter]
+    print(letter, end=' ')
+    for c in range(count):
+        print('|', end='')
+    print()
