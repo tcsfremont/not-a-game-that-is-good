c053c89f502953d6c2d1a59272a3577bbb4daddc
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-08-12 13:04:54 -0700

    encipher

--- encipher.py
+++ encipher.py
@@ -15,12 +15,12 @@ print("output: " + to_filename)
 from_file = open(from_filename, 'r', encoding='utf8')
 to_file = open(to_filename, 'w', encoding='utf8')
 
-contents = from_file .read()
+contents =list(from_file .read())
 
 for index in range(len(contents)):
     letter = contents[index]
     order = ord(letter) - ord('A')
-    if order < 0 or order .= len(KEY):
+    if order < 0 or order >= len(KEY):
         continue
     cipher_letter = KEY[order]
     contents[index] = cipher_letter
@@ -30,7 +30,7 @@ for index in range(len(contents)):
 
 
 
-to_file.write(contents)
+to_file.write(''.join(contents))
 
 from_file.close()
 to_file.close()
